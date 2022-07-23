9144c24fc19a5f73c0204e9a34f18ba584131d5e
Paulogonzalez5679 <paulomga10@gmail.com>
2022-07-23 14:34:28 -0500

    front finalizado

---
+++ src/app/COMPONETES/agregar/agregar.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Tarea, TareaService } from 'src/app/SERVICE/tarea.service';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+  styleUrls: ['./agregar.component.css']
+})
+export class AgregarComponent implements OnInit {
+
+  tareaNueva: Tarea={
+    nombre:'',
+    autor:'',
+    descripcion:'',
+    ejemplares:'',
+    costo:''
+  };
+
+  constructor(private tareaService: TareaService, private router:Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  agregarTarea(){
+    this.tareaService.saveTarea(this.tareaNueva).subscribe(
+      res=>{
+        console.log(res);
+        this.router.navigate(['/inicio']);
+      },
+      err=>console.log(err)
+    );
+  }
+
+}
